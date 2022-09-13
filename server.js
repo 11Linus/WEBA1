3670cf585a34e7347ea471f9e95384ee27205768
Linus <klchan11@mySeneca.ca>
2022-09-13 19:41:59 -0400

    12

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Kin Lok Chan Student ID: 119293215 Date: 9/13/2022
+*  Cyclic Link: https://dizzy-dove-peplum.cyclic.app
+*
+********************************************************************************/ 
+
 require('dotenv').config(); 
 
 const exp = require("express");
