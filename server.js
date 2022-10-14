5faac896a0a78c908754e5c0ee4b428fbdfd0b07
11Linus <95544544+11Linus@users.noreply.github.com>
2022-10-14 16:35:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,102 +9,84 @@
 *
 ********************************************************************************/ 
 
-require('dotenv').config(); 
-
-const exp = require("express");
-const app = exp();
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const cors = require("cors");
-
+const app = express();
 const MoviesDB = require("./modules/moviesDB.js");
 const db = new MoviesDB();
-
-
-
-
-app.use(exp.json())
-app.use(cors());
-
-
-app.get("/",function(req,res){
-    res.json({msg:"API Listening"});
-});
-
-app.post("/api/movies",(req,res)=>{
-   try{
-    const temp =  db.addNewMovie(req.body);
-    res.json(temp);
-   }
-   catch(err){
-        res.status(500).send(err.message);
-    }
-});
-
-app.get("/api/movies",(req,res)=>{
-    db.getAllMovies(req.body.page, req.body.perPage, req.body.title).then(()=>{
-        res.json(req.body);
-    }).catch((err)=>{
-      res.status(500).send("can't add");
-    });
-});
-
-app.get("/api/movies",(req, res)=>{
-    if (req.query.page || req.query.perPage) {
-     try{
-        const temp =  db.getAllMovies(req.query.page, req.query.perPage, req.query.title);
-        res.json(temp);
-     }
-     catch (err){
-        res.status(500).send(err.message);
-     }
-    } else {
-      res.status(404).send(err.message);
-    }
-  });
-
-app.get("/api/movies/:id",(req,res)=>{
-    try{
-        const temp = db.getMovieById(req.params.id);
-        res.json(temp);
-    }
-    catch (err){
-        res.status(204).send(err.message);
-     }
-});
-
-app.put("/api/movies/:id",(req,res)=>{
-    if(req.params.id != req.body){
-        res.status(404).send(err.message);
-    } else{
-        const result = db.updateMovieById(req.body,req.params.id);
-        if(result){
-            res.json(result);
-        } else{
-            res.status(204).send(err.message);
-        }
-    }
-});
-
-app.delete("/api/movies/:id",(req,res)=>{
-    if(req.params.id !== req.body){
-        db.deleteMovieById(req.params.id).then(() => {
-            res.status(201).json({ message: "deleted" });
-          }).catch((err)=>{
-            res.status(500).send(err.message);
-          });
-    } else{
-        res.status(204).send(err.message);
-    }
-});
-
+const dotenv = require('dotenv').config();
 const HTTP_PORT = process.env.PORT || 8080;
 
-//process.env.MONGODB_CONN_STRING
-var connectionString = "mongodb+srv://dbUser:Linus1234@senecaweb.3qtlrcl.mongodb.net/sample_mflix?retryWrites=true&w=majority";
-console.log(connectionString);
-db.initialize(connectionString).then(()=>{
-    app.listen(HTTP_PORT, ()=>{
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Listening' });
+});
+
+app.post("/api/movies", (req, res) => {
+    res.status(201).json(db.addNewMovie(req.body))
+})
+
+app.get("/api/movies", (req, res) => {
+    let page = req.query.page;
+    let perPage = req.query.perPage;
+    let title = req.query.title;
+
+    db.getAllMovies(page, perPage, title).then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json({});
+        });
+})
+
+app.get("/api/movies/:id", (req, res) => {
+    db.getMovieById(req.params.id).then((data) => {
+            res.status(201).json(data);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: 'Something went wrong, please try again ${err}'
+            });
+        });
+});
+
+app.put("/api/movies/:id", (req, res) => {
+    db
+        .updateMovieById(req.body, req.params.id)
+        .then((data) => {
+            res.status(204).json({message: data});
+        })
+        .catch((err) => {
+            res.status(500).json({ message: 'Something went wrong, please try again ${err}' });
+        });
+});
+
+app.delete("/api/movies/:id", (req, res) => {
+    db
+        .deleteMovieById(req.params.id)
+        .then((data) => {
+            res.status(201).json( {message: data });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: 'Something went wrong, please try again ${err}'
+            });
+        });
+});
+
+app.use((req, res) => {
+    res.status(204).send("Resource not found");
+});
+
+db.initialize(process.env.MONGODB_CONN_STRING).then(() => {
+    app.listen(HTTP_PORT, () => {
         console.log(`server listening on: ${HTTP_PORT}`);
     });
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err);
 });
